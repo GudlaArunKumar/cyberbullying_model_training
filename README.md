aa42e0c5669c7069687bd6718b1a22ae904b81d5
GudlaArunKumar <arunkumarakansha@gmail.com>
2024-06-21 13:16:55 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# cyberbulling_detection_project_template 
+
+This project is to implement cyberbullying detection in social media. 
+
+This repository is a standard generic template for further development of this project.
---
+++ cybulldetection/config_schemas/config_schema.py
@@ -0,0 +1,14 @@
+# this is the important file where config store is instantiated
+
+from hydra.core.config_store import ConfigStore 
+from pydantic.dataclasses import dataclass 
+
+
+@dataclass
+class Config:
+    hello: str = "world"
+
+
+def setup_config() -> None:
+    cs = ConfigStore.instance()
+    cs.store(name="config_schema", node=Config)
